52f4b1fa0805dcb4c643d9c9735beec6229a1308
SANGHEEMOON <mshhhhh0000@naver.com>
2025-06-04 03:05:44 +0900

    Feat: train.py, test.py, mlp.py 추가

--- MoonSangHee/data/dataset_preprocessing.py
+++ MoonSangHee/data/dataset_preprocessing.py
@@ -3,29 +3,31 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 
-original = pd.read_csv('./data/HR_Employee_Attrition.csv')
+data = pd.read_csv('./data/gym_churn_us.csv')
 
-filtered_columns = ['Age', 'Attrition', 'EnvironmentSatisfaction', 'JobInvolvement', 'JobLevel', 'JobSatisfaction', 'MaritalStatus', 'MonthlyIncome', 'OverTime', 'StockOptionLevel', 'TotalWorkingYears', 'YearsAtCompany', 'YearsInCurrentRole', 'YearsWithCurrManager']
-object_columns = ['Attrition', 'MaritalStatus', 'OverTime']
-data = original[filtered_columns]
+# filtered_columns = ['Age', 'Attrition', 'EnvironmentSatisfaction', 'JobInvolvement', 'JobLevel', 'JobSatisfaction', 'MaritalStatus', 'MonthlyIncome', 'OverTime', 'StockOptionLevel', 'TotalWorkingYears', 'YearsAtCompany', 'YearsInCurrentRole', 'YearsWithCurrManager']
+# object_columns = ['Attrition', 'MaritalStatus', 'OverTime']
+# data = original[filtered_columns]
 
 
-column_mappings = {}
+# column_mappings = {}
 
-for column in object_columns:
-    mapping = {}
-    if column == 'Attrition' or column == 'OverTime':
-        mapping['Yes'] = 1
-        mapping['No'] = 0
-    else:
-        for idx, content in enumerate(data[column].unique()):
-            mapping[content] = idx
+# for column in object_columns:
+#     mapping = {}
+#     if column == 'Attrition' or column == 'OverTime':
+#         mapping['Yes'] = 1
+#         mapping['No'] = 0
+#     else:
+#         for idx, content in enumerate(data[column].unique()):
+#             mapping[content] = idx
     
-    column_mappings[column] = mapping
-    data[column] = data[column].map(column_mappings[column])
+#     column_mappings[column] = mapping
+#     data[column] = data[column].map(column_mappings[column])
 
-y = data['Attrition']
-X = data.drop(labels='Attrition', axis=1)
+data = data.drop(labels='Phone', axis=1)
+
+y = data['Churn']
+X = data.drop(labels='Churn', axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
 
@@ -35,11 +37,3 @@ test = pd.concat([X_test, y_test], axis=1, ignore_index=False, sort=False)
 train.to_csv('./data/train.csv', index=False)
 test.to_csv('./data/test.csv', index=False)
 
-
-"""
-column_mappings = {
-'Attrition': {'No': 0, 'Yes': 1}, 
-'MaritalStatus': {'Single': 0, 'Married': 1, 'Divorced': 2}, 
-'OverTime': {'No': 0, 'Yes': 1}
-}
-"""
---
+++ MoonSangHee/models/train.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, roc_auc_score, precision_score, recall_score, f1_score
+from xgboost.sklearn import XGBClassifier
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, VotingClassifier
+from lightgbm import LGBMClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+import pickle
+
+model_cfg = {
+    'LogisticRegression': {'class_weight': 'balanced', 'C': 1.0},
+    'XGBClassifier': {'objective': 'binary:logistic'},
+    'LGBMClassifier': {'verbosity' : 1},
+    'save_model' : True
+}
+
+def model_train_val(model, model_cfg, X_train, y_train, X_val, y_val):
+    
+    model_name = model.__class__.__name__
+    if model_name in model_cfg:
+        params = model_cfg[model_name]
+        model.set_params(**params)
+        print(f'Set parameters for {model_name}:{params}')
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_val)
+    precision = precision_score(y_val, y_pred)
+    recall = recall_score(y_val, y_pred)
+    f1 = f1_score(y_val, y_pred)
+    print(f"model's name : {model.__class__.__name__}")
+
+    print(classification_report(y_val, y_pred))
+    print('val_accuracy: ', model.score(X_val, y_val))
+    print('AUC-ROC:', roc_auc_score(y_val, y_pred))
+    print(f'Precision: {precision:.3f}')
+    print(f'Recall: {recall:.3f}')
+    print(f'F1 Score: {f1:.3f}')
+
+    return model
+
+
+
+def main():
+
+    df = pd.read_csv('./data/train.csv')
+
+    X = df.drop('Churn', axis=1)
+    y = df['Churn']
+
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_val_scaled =  scaler.transform(X_val)
+
+    # smote = SMOTE(random_state=42, sampling_strategy = 0.6)
+    # X_train_resampled, y_train_resampled = smote.fit_resample(X_train_scaled, y_train)
+
+    model = LogisticRegression() #LogisticRegression, XGBClassifier, LGBMClassifier, MLPClassifier, DecisionTreeClassifier, KNeighborsClassifier, RandomForestClassifier, SVC
+
+    model = model_train_val(model, model_cfg, X_train_scaled, y_train, X_val_scaled, y_val)
+
+    if model_cfg['save_model'] == True:
+        # print(model.__class__.__name__)
+        dir = './models/' + model.__class__.__name__ + '.pkl'
+        with open(dir, 'wb') as f:
+            pickle.dump(model, f)
+
+        scaler_dir = './models/' + model.__class__.__name__ + '_scaler.pkl'
+        with open(scaler_dir, 'wb') as s:
+          pickle.dump(scaler, s)
+            
+
+
+    
+
+if __name__ == '__main__':
+    main()
